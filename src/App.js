42cf997a6438254cf4b9c4d3e6afe282f01d4c2a
ndrdul <nathandrdul@gmail.com>
2020-08-06 05:45:22 -0400

    your message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+<div class="wrapper">
+  <div class="box a">A</div>
+  <div class="box b">B</div>
+  <div class="box c">C</div>
+  <div class="box d">D</div>
+  <div class="box e">E</div>
+  <div class="box f">F</div>
+  <div class="box g">G</div>
+  <div class="box h">H</div>
+  <div class="box i">I</div>
+  <div class="box j">J</div>
+  <div class="box k">K</div>
+  <div class="box l">L</div>
+  <div class="box m">M</div>
+  <div class="box n">N</div>
+  <div class="box o">O</div>
+  <div class="box p">P</div>
+  <div class="box q">Q</div>
+  <div class="box r">R</div>
+  <div class="box s">S</div>
+  <div class="box t">T</div>
+  <div class="box u">U</div>
+  <div class="box v">V</div>
+  <div class="box w">W</div>
+  <div class="box x">X</div>
+  <div class="box y">Y</div>
+  <div class="box y">Z</div>
+  <div class="box end">end</div>
+</div>
+
   );
 }
 
